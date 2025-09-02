df85f848ee627c3ac5fa07989ac50bbde3ce28ed
Donatus <Donaemeka92@gmail.com>
2025-09-02 10:39:36 +0200

    fix: create terraform backend resources

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-eu-west-2"
+    bucket         = "my-new-terraform-bucket"  # ← Match this
     key            = "monitor-project/terraform.tfstate"
     region         = "eu-west-2" 
     dynamodb_table = "terraform-state-lock"
