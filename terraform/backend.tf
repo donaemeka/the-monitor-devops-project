c60080b1ff92b20ab4aa2f71ea8f81132c44d9b5
Donatus <Donaemeka92@gmail.com>
2025-09-02 09:51:13 +0200

    Update deploy workflow and backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-eu-west-2"
     key            = "monitor-project/terraform.tfstate"
-    region         = "eu-west-2"  # Your preferred region
+    region         = "eu-west-2" 
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
